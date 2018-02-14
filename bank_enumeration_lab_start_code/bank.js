c8d9933015d6c75c773e96964ff9af1b84cdbb6f
Alison Wood <alisonwood29@gmail.com>
2018-02-14 15:18:19 +0000

    added function for paying 10% interest to each account, test passing

--- bank_enumeration_lab_start_code/bank.js
+++ bank_enumeration_lab_start_code/bank.js
@@ -14,13 +14,21 @@ Bank.prototype.accountByName = function (name) {
 }
 
 Bank.prototype.largestAccount = function () {
-  const highestValue = this.accounts.reduce(function(a, account) {
-    return Math.max(a, account.value);
+  const highestValue = this.accounts.reduce(function(accumulator, account) {
+    return Math.max(accumulator, account.value);
   }, 0)
   const highestValueAccount = this.accounts.filter(function(account) {
     return account.value === highestValue;
   })
   return highestValueAccount[0];
+}
+
+Bank.prototype.payInterest = function () {
+  const addedInterest = this.accounts.map(function (account) {
+    account.value *= 1.1;
+    return account;
+  })
+  this.accounts = addedInterest;
 }
 
 module.exports = Bank;
